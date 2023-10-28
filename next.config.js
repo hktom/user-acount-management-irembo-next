9ab6093cc1f473fd975d52083ad4f808f82fe480
tom hikari <tomprescott1565@gmail.com>
2023-10-28 17:04:35 +0100

    install new dependcies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // page extension
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js', 'page.mdx', 'page.md'],
 }
 
 module.exports = nextConfig
