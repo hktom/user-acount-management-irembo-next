e26bf89744d793ca217d498a01b5000051e1872c
tom hikari <tomprescott1565@gmail.com>
2023-10-29 11:18:01 +0100

    feature middleware

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -4,13 +4,27 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   let cookie = request.cookies.get("token");
-  if (cookie) {
-    return NextResponse.next();
+
+  if (request.nextUrl.pathname.startsWith("/auth") && cookie) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
-  return NextResponse.redirect(new URL("/auth/login", request.url));
+
+  if (request.nextUrl.pathname == "/" && !cookie) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  if (
+    (request.nextUrl.pathname.startsWith("/admin") ||
+      request.nextUrl.pathname.startsWith("/user")) &&
+    !cookie
+  ) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: "/",
+  matcher: ["/", "/user/:path*", "/auth/:path*", "/admin/:path*"],
 };
