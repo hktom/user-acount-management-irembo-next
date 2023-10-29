eb0e4696f97b88b305b89ff312622c787de53372
tom hikari <tomprescott1565@gmail.com>
2023-10-29 10:57:59 +0100

    config stores

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,12 +1,15 @@
+import store from "@/config/store";
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
-
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider store={store}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
