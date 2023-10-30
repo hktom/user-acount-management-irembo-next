9d6dde40da690769671f15b502132f12b1754f18
tom hikari <tomprescott1565@gmail.com>
2023-10-30 21:03:33 +0100

    feature jest

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    moduleDirectories: ['node_modules', 'src'],
-    testMatch: ['<rootDir>/src/**/*.test.ts'],
-    'moduleNameMapper': {
-        '@/(.*)': '<rootDir>/src/$1'
-   },
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '^.+\\.ts$': 'babel-jest',
-    },
+  collectCoverageFrom: ["src/**/*.ts"],
+  moduleDirectories: ["node_modules", "src"],
+  testMatch: ["<rootDir>/src/**/*.test.ts"],
+  moduleNameMapper: {
+    "@/(.*)": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.ts$": "babel-jest",
+  },
 };
